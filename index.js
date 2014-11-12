f6eba440fe28e5f5b5032f40877f600aaf253875
Andre Bauer <bauer@schneide.com>
2014-11-12 17:39:02 +0100

    Added todo ical-todo-creation-test.js

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
-var ical = require('./lib/ical');
+var ical = require('./lib/ical'),
+    uuid = require('node-uuid');
 
+var map = {};
+
+var getAllOpenTodos = function(input,until){
+    if(!until.instanceOf(Date)) {
+        throw 'Second parameter has to be a date'
+    }
+    var component = ical.Component(ical.parse(input));
+    var vtodos = component.getAllSubcomponents('vtodo');
+    var rrule = vtodos.getFirstPropertyValue("rrule");
+    var dtstart = vtodos.getFirstPropertyValue("dtstart");
+    var iter = rrule.iterator(dtstart);
+    var next, result = [];
+    while(next = iter.next()){
+        var  existingtodo;
+        if(existingtodo = getTodoWithRecurrenceId(vtodos,next)) {
+            result.push(existingtodo);
+        }
+    }
+    return result
+
+};
+
+var getTodoWithRecurrenceId =  function(todos,recurrenceId) {
+    for (var i = 0; i < todos.length; i++) {
+        var val = todos[i];
+        if(!val.getFirstProperty('recurrence-id')) {
+            continue;
+        }
+        if(val.getFirstProperty('recurrence-id').getFirstValue().toICALString() === recurrenceId.toICALString()) {
+            return val;
+        }
+    }
+    return null;
+};
+
+function createTodo(vtodo) {
+
+}
+
+module.exports = {
+    getAllOpenTodos:getAllOpenTodos,
+    getTodoWithRecurrenceId:getTodoWithRecurrenceId
+};
---
+++ test/ical-todo-creation-test.js
@@ -0,0 +1,18 @@
+/**
+ * Created by aab on 12.11.2014.
+ */
+var should = require('chai').should(),
+    icalTodo = require('../index'),
+    ical = require('../lib/ical'),
+    fs = require('fs');
+
+var ics = fs.readFileSync('./test/test-ics/79fe94ee-5524-46bd-b5e6-4fdec65c1f01-ein-mal-erledigt.ics',{encoding :'utf8'});
+
+describe('Test ical-todo todo creation methods', function() {
+    it('getTodoWithRecurrenceId should find correct todo', function() {
+        var comp = new ical.Component(ical.parse(ics));
+        var vtodos = comp.getAllSubcomponents('vtodo');
+        var vtodo = icalTodo.getTodoWithRecurrenceId(vtodos,ical.Time.fromJSDate(new Date(2014,10,12,10,00,00)));
+        vtodo.should.equal(vtodos[1]);
+    });
+});
