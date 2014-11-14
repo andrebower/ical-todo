fb4cd37efb6265ca79b6cc7a1c674ef641753b1c
André Bauer <spamadresse23@web.de>
2014-11-14 15:59:19 +0100

    Just some renaming

--- test/todo-repository-test.js
+++ test/todo-repository-test.js
@@ -13,12 +13,12 @@ chai.use(require('chai-datetime'));
 describe('Test todo-repository', function () {
     it('test map single instance', function () {
         var caldavRequesterMock = {};
-        todoRepo = proxyquire('../lib/ical-todo/todo-repository', {'./caldav-requester': caldavRequesterMock});
+        var todoRepoWithMockedRequester = proxyquire('../lib/ical-todo/todo-repository', {'./caldav-requester': caldavRequesterMock});
         caldavRequesterMock.getIcsData = function (url) {
             var result = [];
             result.push(fs.readFileSync('./test/test-ics/79fe94ee-5524-46bd-b5e6-4fdec65c1f01-completed-once.ics'));
             return result;
         };
-        todoRepo.getAllTodos().should.be.instanceof(Array);
+        todoRepoWithMockedRequester.getAllTodos().should.be.instanceof(Array);
     });
 });
