d3933bb9a3652fd28c6d8408cbfeef35957f76d5
André Bauer <spamadresse23@web.de>
2014-11-14 15:56:11 +0100

    added proxyquire to ease mocking required modules a lot!

--- test/todo-repository-test.js
+++ test/todo-repository-test.js
@@ -5,14 +5,20 @@ var chai = require('chai'),
     should = chai.should(),
     todoRepo = require('../lib/ical-todo/todo-repository'),
     ical = require('../lib/ical'),
-    fs = require('./test-helper/fs-test-helper');
+    fs = require('./test-helper/fs-test-helper'),
+    proxyquire = require('proxyquire');
 chai.use(require('chai-datetime'));
 
 
-describe('Test todo-repository', function() {
-    it('test map single instance', function() {
-        todoRepo.set('a','a');
-        var rep = require('../lib/ical-todo/todo-repository');
-        rep.get('a').should.equal('a');
+describe('Test todo-repository', function () {
+    it('test map single instance', function () {
+        var caldavRequesterMock = {};
+        todoRepo = proxyquire('../lib/ical-todo/todo-repository', {'./caldav-requester': caldavRequesterMock});
+        caldavRequesterMock.getIcsData = function (url) {
+            var result = [];
+            result.push(fs.readFileSync('./test/test-ics/79fe94ee-5524-46bd-b5e6-4fdec65c1f01-completed-once.ics'));
+            return result;
+        };
+        todoRepo.getAllTodos().should.be.instanceof(Array);
     });
 });
