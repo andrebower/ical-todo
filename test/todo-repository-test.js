d77cd5c3365e2d6f381c2ede8df30544fdcc1317
André Bauer <spamadresse23@web.de>
2014-11-25 17:30:39 +0100

    changed test of todo-repository to work async

--- test/todo-repository-test.js
+++ test/todo-repository-test.js
@@ -10,41 +10,46 @@ chai.use(require('chai-datetime'));
 
 
 describe('Test todo-repository', function () {
-    it('test if mocking of caldav-requester works', function () {
+    it('test if mocking of caldav-requester works', function (done) {
         var caldavRequesterMock = {};
         var todoRepoWithMockedRequester = proxyquire('../lib/ical-todo/todo-repository', {'./caldav-requester': caldavRequesterMock})('');
-        caldavRequesterMock.getIcsData = function (url) {
+        caldavRequesterMock.getIcsData = function (url, callback) {
             var result = [];
             result.push(fs.readFileSync('./test/test-ics/79fe94ee-5524-46bd-b5e6-4fdec65c1f01-completed-once.ics'));
-            return result;
+            callback(result);
         };
-        todoRepoWithMockedRequester.getAllTodos(new Date(2014,11,29,11)).should.be.instanceof(Array);
+        todoRepoWithMockedRequester.getAllTodos(new Date(2014, 11, 29, 11),function(result){
+            var expected = {uuid: '79fe94ee-5524-46bd-b5e6-4fdec65c1f01-20141112T100000', summary: 'Aufgabe die 1 mal erledigt ist', dueDate: new Date(2014, 10, 12, 10), completed: true};
+            result.should.be.instanceof(Array);
+            done();
+        });
     });
-    it('test getAllTodos first is completed', function () {
+    it('test getAllTodos first is completed', function (done) {
         var caldavRequesterMock = {};
         var todoRepoWithMockedRequester = proxyquire('../lib/ical-todo/todo-repository', {'./caldav-requester': caldavRequesterMock})('');
-        caldavRequesterMock.getIcsData = function (url) {
+        caldavRequesterMock.getIcsData = function (url, callback) {
             var result = [];
             result.push(fs.readFileSync('./test/test-ics/79fe94ee-5524-46bd-b5e6-4fdec65c1f01-completed-once.ics'));
-            return result;
+            callback(result);
         };
-
-        var firstCompleted = todoRepoWithMockedRequester.getAllTodos(new Date(2014,11,29,11))[0];
-        var expected = {uuid:'79fe94ee-5524-46bd-b5e6-4fdec65c1f01-20141112T100000',summary:'Aufgabe die 1 mal erledigt ist',dueDate:new Date(2014,10,12,10),completed:true};
-        firstCompleted.should.eql(expected);
+        todoRepoWithMockedRequester.getAllTodos(new Date(2014, 11, 29, 11),function(result){
+            var expected = {uuid: '79fe94ee-5524-46bd-b5e6-4fdec65c1f01-20141112T100000', summary: 'Aufgabe die 1 mal erledigt ist', dueDate: new Date(2014, 10, 12, 10), completed: true};
+            result[0].should.eql(expected);
+            done();
+        });
     });
-    it('test getAllTodos fifthOpen is correct', function () {
+    it('test getAllTodos fifthOpen is correct', function (done) {
         var caldavRequesterMock = {};
         var todoRepoWithMockedRequester = proxyquire('../lib/ical-todo/todo-repository', {'./caldav-requester': caldavRequesterMock})('');
-        caldavRequesterMock.getIcsData = function (url) {
+        caldavRequesterMock.getIcsData = function (url, callback) {
             var result = [];
             result.push(fs.readFileSync('./test/test-ics/79fe94ee-5524-46bd-b5e6-4fdec65c1f01-completed-once.ics'));
-            return result;
+            callback(result);
         };
-
-        var fifthOpen = todoRepoWithMockedRequester.getAllTodos(new Date(2014,11,29,11))[5];
-        var expected = {uuid:'79fe94ee-5524-46bd-b5e6-4fdec65c1f01-20141117T100000',summary:'Aufgabe die 1 mal erledigt ist',dueDate:new Date(2014,10,17,10),completed:false};
-        fifthOpen.should.eql(expected);
+        todoRepoWithMockedRequester.getAllTodos(new Date(2014, 11, 29, 11),function(result){
+            var expected = {uuid: '79fe94ee-5524-46bd-b5e6-4fdec65c1f01-20141117T100000', summary: 'Aufgabe die 1 mal erledigt ist', dueDate: new Date(2014, 10, 17, 10), completed: false};
+            result[5].should.eql(expected);
+            done();
+        });
     });
-
 });
